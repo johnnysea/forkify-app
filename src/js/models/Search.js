f036b225ee11158ebb4c8f413e16c3de181766e7
Johnny Shao <39663313+johnnysea@users.noreply.github.com>
2020-05-27 18:39:25 +1200

    build Search Controller

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -8,7 +8,7 @@ export default class Search {
     async getResults(query) {
         try {
             const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`);
-            this.result = res.data.recipes;
+            this.recipes = res.data.recipes;
         } catch (error) {
             alert(error);
         }
