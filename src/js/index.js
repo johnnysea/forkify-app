1ef9bb5ce5023f23ec065c78695ff2073a163da6
Johnny Shao <39663313+johnnysea@users.noreply.github.com>
2020-05-28 17:21:56 +1200

    initialise Recipe Model

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import { elements, renderLoader, clearLoader } from './views/base';
 import * as searchView from './views/searchView';
 
@@ -10,6 +11,9 @@ import * as searchView from './views/searchView';
  */
 const state = {};
 
+/**
+ * Search controller
+ */
 const controlSearch = async () => {
     // 1) Get the query from the view
     const query = searchView.getInput();
@@ -23,14 +27,50 @@ const controlSearch = async () => {
         searchView.clearResults();
         renderLoader(elements.searchRes);
 
-        // 4) Search for recipes
-        await state.search.getResults();
+        try {
+            // 4) Search for recipes
+            await state.search.getResults();
 
-        // 5) render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
+            // 5) render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch (err) {
+            alert('Something wrong with the search...');
+        }
     }
 }
+
+/**
+ * Recipe controller
+ */
+const controlRecipe = async () => {
+    // Get ID from url
+    const id = window.location.hash.replace('#', '');
+
+    if (id) {
+        // Prepare UI for changes
+
+        // Create new recipe object
+        state.recipe = new Recipe(id);
+
+        try {
+            // Get recipe data
+            await state.recipe.getRecipe();
+
+            // Calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            // Render recipe
+            console.log(state.recipe);
+        } catch (err) {
+            alert('Error processing recipe!');
+        }
+    }
+}
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
 
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
